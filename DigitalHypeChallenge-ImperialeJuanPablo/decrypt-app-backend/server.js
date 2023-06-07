460da097a0263399edd283881a4fc83cae75b0ae
Juanpi Imperiale <56052268+JuanPabloImperiale@users.noreply.github.com>
2023-06-07 16:33:28 -0500

    Update server.js

--- DigitalHypeChallenge-ImperialeJuanPablo/decrypt-app-backend/server.js
+++ DigitalHypeChallenge-ImperialeJuanPablo/decrypt-app-backend/server.js
@@ -9,13 +9,20 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/decrypt", (req, res) => {
-  const { encryptedMessage } = req.body;
-  const decryptedMessage = decryptMessage(encryptedMessage);
-  // Save the decrypted message in MongoDB
-  saveDecryptedMessage(decryptedMessage);
-  res.json({ decryptedMessage });
-});
+const connectToMongoDB = async () => {
+  try {
+    const client = await MongoClient.connect(
+      "mongodb://root:123456789@localhost:27017/DecryptDigitalHype?authSource=admin",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const db = client.db("DecryptDigitalHype");
+    const collection = db.collection("decrypted_messages");
+    await collection.insertOne(decryptedMessage);
+    client.close();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const decryptMessage = (encryptedMessage) => {
   // considering a dynamic number of zeros
@@ -29,18 +36,18 @@ const decryptMessage = (encryptedMessage) => {
   };
 };
 
-const client = await MongoClient.connect(
-      "mongodb://root:123456789@localhost:27017/DecryptDigitalHype?authSource=admin",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    const db = client.db("DecryptDigitalHype");
-    const collection = db.collection("decrypted_messages");
-    await collection.insertOne(decryptedMessage);
-    client.close();
+app.post("/decrypt", async (req, res) => {
+  const { encryptedMessage } = req.body;
+  const decryptedMessage = decryptMessage(encryptedMessage);
+
+  try {
+    await connectToMongoDB(decryptedMessage);
+    res.status(200).json({ decryptedMessage }); // Código de estado 200 (OK)
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "Internal Server Error" }); // Código de estado 500 (Error interno del servidor)
   }
-};
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
